c7a599f57256f65be8658d12c3bbd65b4457f2b1
nepal143 <nepalsss007@gmail.com>
2025-01-17 22:03:44 +0530

    added walk animation and controller script

---
+++ Assets/Scripts/Handgun/HandgunAnimationController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class HandgunAnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        // Get the Animator component attached to this GameObject
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("Animator component not found on " + gameObject.name);
+        }
+    }
+
+    void Update()
+    {
+        // Check if any movement key is pressed 
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || 
+            Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) ||
+            Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || 
+            Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            // Trigger walk animation
+            animator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            // Stop walk animation
+            animator.SetBool("IsWalking", false);
+        }
+    }
+}
